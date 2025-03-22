e71e076ae725bc22ac380586c7aabc889f0d673e
Timmotal <timmortal44@yahoo.com>
2025-03-22 21:16:03 +0000

    add needed transition

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -34,7 +34,7 @@ export default function Nav() {
         
         <li><Link href="#">Gallery</Link></li>
         <li><Link href="#">Contact</Link></li>
-        <li><Link href="#" className="border px-4 py-2 rounded hover:bg-white hover:text-black">Book Now</Link></li>
+        <li><Link href="#" className="border px-4 py-2 rounded hover:bg-white hover:text-black transition-all duration-300">Book Now</Link></li>
       </ul>
     </nav>
   );
